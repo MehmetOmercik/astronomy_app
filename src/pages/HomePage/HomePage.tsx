7535ef4781559896a8ed88ffdb25cab6b1402d60
Mehmet Omercik <mehmetomercik@hotmail.co.uk>
2023-07-03 10:22:04 +0100

    Basic info on homepage

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,12 +1,18 @@
 import { FC } from "react";
 
-// import Particle from "../../particle";
-
 export const HomePage: FC = () => {
   return (
-    <>
-      {/* <Particle /> */}
-      <p>Home Page</p>
-    </>
+    <div>
+      <section className="mt-16 text-center text-xl leading-9 sm:container">
+        <p>Home Page</p>
+        <p>Hello, welcome to the homepage of my astronomy web application.</p>
+        <p>
+          This application is connected to the astronomy API so that it can collect vital
+          information about celestial bodies in the universe.
+        </p>
+        <p>Please feel free to explore the page </p>
+        <p>I hope you learn alot more about the Universe around you from reading this website </p>
+      </section>
+    </div>
   );
 };
