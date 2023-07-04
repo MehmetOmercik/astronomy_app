b5368e9eaefb6404f6f31daf5bbdf5c0d15f1b30
Mehmet Omercik <mehmetomercik@hotmail.co.uk>
2023-07-04 22:15:19 +0100

    Attempt at creating absolute filepaths

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,18 @@
 /** @type {import('jest').Config} */
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@app/*": path.resolve(__dirname, "./src/app"),
+    },
+  },
   plugins: [react()],
   test: {
     globals: true,
