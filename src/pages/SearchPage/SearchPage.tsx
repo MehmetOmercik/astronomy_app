49313a6b9717802af6a466f10cd2a698c809cfca
Mehmet Omercik <mehmetomercik@hotmail.co.uk>
2023-07-25 14:15:18 +0100

    Created a table for SearchPage

--- src/pages/SearchPage/SearchPage.tsx
+++ src/pages/SearchPage/SearchPage.tsx
@@ -1,16 +1,63 @@
 import React, { FC } from "react";
-import { useAppSelector } from "@/app/hooks";
+import { useAppSelector } from "@app/hooks";
 
+interface SearchPageProps {
+  name: string;
+  type: {
+    id: string;
+    name: string;
+  };
+  subType: {
+    id: string;
+    name: string;
+  };
+  crossIdentification: object[];
+  position: {
+    constellation: {
+      id: string;
+      short: string;
+      name: string;
+    };
+    equatorial: {
+      declination: {
+        degress: string;
+        string: string;
+      };
+      rightAscension: {
+        degress: string;
+        string: string;
+      };
+    };
+  };
+}
 export const SearchPage: FC = () => {
-  const { name, type, subType, crossIdentification } = useAppSelector(
-    (state) => state.search.searchCurrent
-  );
+  const searchCurrent: SearchPageProps = useAppSelector((state) => state.search.searchCurrent);
+  const { name, type, subType, crossIdentification, position } = searchCurrent;
 
   return (
-    <div>
-      <h1>{name}</h1>
-      <h1>{type.name}</h1>
-      <h1>{subType.name}</h1>
+    <div className="flex flex-col">
+      <h1 className="my-4 text-center text-4xl text-yellow-500">{name}</h1>
+      <table className="self-center">
+        <tbody>
+          <tr>
+            <td className="solar-system-table-data">Name</td>
+            <td className="solar-system-table-data">Type</td>
+            <td className="solar-system-table-data">Subtype</td>
+            <td className="solar-system-table-data">Constellation</td>
+            <td className="solar-system-table-data">Position</td>
+          </tr>
+          <tr>
+            <td className="solar-system-table-data">{name}</td>
+            <td className="solar-system-table-data">{type?.name}</td>
+            <td className="solar-system-table-data">{subType?.name}</td>
+            <td className="solar-system-table-data">{position?.constellation.name}</td>
+            <td className="solar-system-table-data">
+              {position?.equatorial.rightAscension.string} RA and{" "}
+              {position?.equatorial.declination.string} DEC
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 };
