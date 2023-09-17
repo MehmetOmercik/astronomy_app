548c9401e4b4410ee76fbca7dd910c6bf93f2c9a
Mehmet Omercik <mehmetomercik@hotmail.co.uk>
2023-09-17 14:07:02 +0100

    Hiding the search bar for now until debouncing is implemented

--- src/components/UI/NavBar/NavBar.tsx
+++ src/components/UI/NavBar/NavBar.tsx
@@ -3,8 +3,9 @@ import { LinkSimple } from "../indexUI";
 import { SearchBar } from "../indexUI";
 export const NavBar: FC = () => {
   return (
-    <nav className="flex flex-col gap-y-2 bg-gray-700 py-4">
-      <SearchBar />
+    <nav className="flex min-w-[150px] flex-col gap-y-2 bg-gray-700 py-4">
+      {/* //TODO Implement debouncing on search bar to prevent spamming requests to backend */}
+      {/* <SearchBar /> */}
       <div className="flex w-full flex-col">
         <LinkSimple value="Home" to="/" className="navbar-button" />
         <LinkSimple value="Solar System" to="/solarSystem" className="navbar-button" />
