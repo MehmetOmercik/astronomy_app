f0bae3b201309a46953f7af892488bb929ea7e73
Mehmet Omercik <mehmetomercik@hotmail.co.uk>
2023-09-17 14:04:05 +0100

    Removed the outdated capitalised folders

--- src/Components/UI/indexUI.ts
+++
@@ -1,9 +0,0 @@
-export { SearchBar } from "./SearchBar/Searchbar";
-export { NavBar } from "./NavBar/NavBar";
-
-//Buttons
-export { LinkSimple } from "./Buttons/Link/Link";
-export { DropdownWithLabel } from "./Buttons/DropdownWithLabel/DropdownWithLabel";
-
-//Table
-export { SolarSystemBodyTable } from "./Tables/SolarSystemBodyTable/SolarSystemBodyTable";
