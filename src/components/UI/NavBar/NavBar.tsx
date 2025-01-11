be5163fdd9be5b35599fc65a86548b077e1b3399
Mehmet Omercik <mehmetomercik@hotmail.co.uk>
2025-01-11 17:23:03 +0000

    feat: :lipstick: Added hamburger button and made sidebar dynamic for responsiveness

--- src/components/UI/NavBar/NavBar.tsx
+++ src/components/UI/NavBar/NavBar.tsx
@@ -1,17 +1,31 @@
-import { FC } from "react";
+import { FC, Fragment, useState } from "react";
 import { LinkSimple } from "../indexUI";
 // import { SearchBar } from "../indexUI";
 export const NavBar: FC = () => {
+  const [isSidebarVisible, setIsSidebarVisible] = useState(true);
+
   return (
-    <nav className="flex min-w-[150px] flex-col gap-y-2 bg-gray-700 py-4">
-      {/* //TODO Implement debouncing on search bar to prevent spamming requests to backend */}
-      {/* <SearchBar /> */}
-      <div className="flex w-full flex-col">
-        <LinkSimple value="Home" to="/" className="navbar-button" />
-        <LinkSimple value="Solar System" to="/solarSystem" className="navbar-button" />
-        <LinkSimple value="Star Chart" to="/starChart" className="navbar-button" />
-        <LinkSimple value="Moon Phases" to="/moonPhase" className="navbar-button" />
+    <Fragment>
+      {/* Hamburger button */}
+      {!isSidebarVisible && 
+      <div className="hamburger-container" onClick={() => {setIsSidebarVisible(true)}}>
+        <div className={`hamburger-bar`}></div>
+        <div className={`hamburger-bar`}></div>
+        <div className={`hamburger-bar`}></div>
       </div>
-    </nav>
+        
+      }
+      <nav className={`sidebar ${isSidebarVisible ? 'open' : 'closed'}`}>
+        {/* //TODO Implement debouncing on search bar to prevent spamming requests to backend */}
+        {/* <SearchBar /> */}
+        <div className="flex w-full flex-col relative pt-9">
+          <button className="absolute top-1 right-4 text-2xl" onClick={() => setIsSidebarVisible(false)}>&lt;&lt;</button>
+          <LinkSimple value="Home" to="/" className="navbar-button" />
+          <LinkSimple value="Solar System" to="/solarSystem" className="navbar-button" />
+          <LinkSimple value="Star Chart" to="/starChart" className="navbar-button" />
+          <LinkSimple value="Moon Phases" to="/moonPhase" className="navbar-button" />
+        </div>
+      </nav>
+    </Fragment>
   );
 };
