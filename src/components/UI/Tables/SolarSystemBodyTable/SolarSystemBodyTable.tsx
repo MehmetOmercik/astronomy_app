a95cf8067f6c9acc6ea8b5f3c9a398e25ad4402c
Mehmet Omercik <mehmetomercik@hotmail.co.uk>
2025-01-13 20:48:22 +0000

    feat: :lipstick: Changed solar system table to grid layout
    * This was changed for the purposees of responsiveness. Could be a temporary change but for now
    I will leave it like this.

--- src/components/UI/Tables/SolarSystemBodyTable/SolarSystemBodyTable.tsx
+++ src/components/UI/Tables/SolarSystemBodyTable/SolarSystemBodyTable.tsx
@@ -1,4 +1,4 @@
-import { FC, HTMLAttributes } from "react";
+import { FC, Fragment, HTMLAttributes } from "react";
 
 interface SolarSystemBodyTableProps extends HTMLAttributes<HTMLTableElement> {
   headers: [];
@@ -43,47 +43,74 @@ export const SolarSystemBodyTable: FC<SolarSystemBodyTableProps> = ({
   ...props
 }) => {
   return (
-    <table {...props}>
-      <thead className="">
-        <tr className="solar-system-table-header">
-          <th className=""></th>
-        </tr>
-        {headers.map((header: Date, index: number) => {
-          const date = new Date(header).toLocaleDateString();
-          return (
-            <tr key={index} className="solar-system-table-header flex justify-center">
-              <th className="text-center">{date}</th>
-            </tr>
-          );
-        })}
-      </thead>
-      <tbody className="">
-        <tr className="">
-          <td className="solar-system-table-column">Distance from Earth</td>
-          <td className="solar-system-table-column">Position</td>
-          <td className="solar-system-table-column">Elongation</td>
-          <td className="solar-system-table-column">Magnitude</td>
-        </tr>
-        {/*//TODO make rowProps work with type row */}
-        {rows.map((row: any, index: number) => (
-          <tr key={index}>
-            <td className="solar-system-table-data">
-              {row.distance.fromEarth.au} au / {row.distance.fromEarth.km} km
-            </td>
-            <td className="solar-system-table-data">
-              {row.position.equatorial.rightAscension.string} rh AND{" "}
-              {row.position.equatorial.declination.string}
-              dec
-            </td>
-            <td className="solar-system-table-data">
-              {row.extraInfo.elongation ? row.extraInfo.elongation : "null"}
-            </td>
-            <td className="solar-system-table-data">
-              {row.extraInfo.magnitude ? row.extraInfo.magnitude : "null"}
-            </td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
+    // <table {...props}>
+    //   <thead className="">
+    //     <tr className="solar-system-table-header">
+    //       <th className=""></th>
+    //     </tr>
+    //     {headers.map((header: Date, index: number) => {
+    //       const date = new Date(header).toLocaleDateString();
+    //       return (
+    //         <tr key={index} className="solar-system-table-header flex justify-center">
+    //           <th className="text-center">{date}</th>
+    //         </tr>
+    //       );
+    //     })}
+    //   </thead>
+    //   <tbody className="">
+    //     <tr className="">
+    //       <td className="solar-system-table-column">Distance from Earth</td>
+    //       <td className="solar-system-table-column">Position</td>
+    //       <td className="solar-system-table-column">Elongation</td>
+    //       <td className="solar-system-table-column">Magnitude</td>
+    //     </tr>
+    //     {/*//TODO make rowProps work with type row */}
+    //     {rows.map((row: any, index: number) => (
+    //       <tr key={index}>
+    //         <td className="solar-system-table-data">
+    //           {row.distance.fromEarth.au} au / {row.distance.fromEarth.km} km
+    //         </td>
+    //         <td className="solar-system-table-data">
+    //           {row.position.equatorial.rightAscension.string} rh AND{" "}
+    //           {row.position.equatorial.declination.string}
+    //           dec
+    //         </td>
+    //         <td className="solar-system-table-data">
+    //           {row.extraInfo.elongation ? row.extraInfo.elongation : "null"}
+    //         </td>
+    //         <td className="solar-system-table-data">
+    //           {row.extraInfo.magnitude ? row.extraInfo.magnitude : "null"}
+    //         </td>
+    //       </tr>
+    //     ))}
+    //   </tbody>
+    // </table>
+    // TODO New responsive table (possibly temporary), check if you can increase columns...
+    <section className="grid grid-cols-2 p-4">
+      {headers.map((header: Date, index: number) => {
+        const date = new Date(header).toLocaleDateString();
+        return (
+          <Fragment key={`solar-system-header-row-${index}`}>
+            <div className="solar-system-table-header rounded-tl-xl"></div>
+            <div className="solar-system-table-header rounded-tr-xl">{date}</div>
+          </Fragment>
+          
+        );
+      })}
+      {rows.map((row: any, index: number) => (
+        <Fragment key={`solar-system-row-${index}`}>
+          <div className="solar-system-table-data">Distance from Earth</div>
+          <div className="solar-system-table-data">{row.distance.fromEarth.au} au / {row.distance.fromEarth.km} km</div>
+          <div className="solar-system-table-data">Position</div>
+          <div className="solar-system-table-data">{row.position.equatorial.rightAscension.string} rh AND{" "}
+               {row.position.equatorial.declination.string}
+          </div>
+          <div className="solar-system-table-data">Elongation</div>
+          <div className="solar-system-table-data">{row.extraInfo.elongation ? row.extraInfo.elongation : "null"}</div>
+          <div className="solar-system-table-data rounded-bl-xl">Magnitude</div>
+          <div className="solar-system-table-data rounded-br-xl">{row.extraInfo.magnitude ? row.extraInfo.magnitude : "null"}</div>
+        </Fragment>
+      ))}
+    </section>
   );
 };
