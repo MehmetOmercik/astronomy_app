6df5b0f713630ffafaa913f7b5a911b6306429db
Mehmet Omercik <mehmetomercik@hotmail.co.uk>
2025-01-14 18:23:52 +0000

    feat: :sparkles: Added react-icons and back button to solar system table

--- src/pages/SolarSystemBodyPage/SolarSystemBodyPage.tsx
+++ src/pages/SolarSystemBodyPage/SolarSystemBodyPage.tsx
@@ -1,6 +1,10 @@
+import { Fragment } from "react";
+import { IoArrowBackCircle } from "react-icons/io5";
+
 import { SolarSystemState } from "@features/SolarSystem/SolarSystemSlice";
 import { useAppSelector } from "@app/hooks";
 import { SolarSystemBodyTable } from "@components/UI/indexUI";
+import { useNavigate } from "react-router-dom";
 
 // TODO Add EVENTS API to page!
 export const SolarSystemBodyPage: React.FC = () => {
@@ -15,7 +19,6 @@ export const SolarSystemBodyPage: React.FC = () => {
   // }
 
   // TODO get solarSystemState working for below
-  // TODO Make table more responsive!
   const {
     title,
     // description,
@@ -24,18 +27,27 @@ export const SolarSystemBodyPage: React.FC = () => {
   } = useAppSelector((state: any) => state.solarSystem);
   const headers = table.header;
   const rows = table.rows[0].cells;
+  const navigate = useNavigate();
 
   return (
-    <div className="container my-8 flex min-w-[calc(100%-150px)] justify-center">
-      {solarSystemState === SolarSystemState.PENDING && <h1>Loading, please wait...</h1>}
-      {solarSystemState === SolarSystemState.FULFILLED && (
-        <div>
-          <h1 className="mb-4 text-2xl text-center font-medium">{title}</h1>
-          {/* <p className="">{description}</p> */}
-          <SolarSystemBodyTable className="" headers={headers} rows={rows} />
-        </div>
-      )}
-      {solarSystemState === SolarSystemState.REJECTED && <p>ERROR: NOT LOADING</p>}
-    </div>
+    <Fragment>
+      <button 
+        className="absolute top-12 left-1 text-5xl min-[345px]:top-3 min-[345px]:left-12 sm:top-2 sm:left-32 sm:text-7xl"
+        onClick={() => navigate('/solarSystem')}
+      >
+        <IoArrowBackCircle />
+      </button>
+      <section className="my-8 flex flex-grow justify-center">
+        {solarSystemState === SolarSystemState.PENDING && <h1>Loading, please wait...</h1>}
+        {solarSystemState === SolarSystemState.FULFILLED && (
+          <div>
+            <h1 className="mb-4 text-3xl text-center font-medium">{title}</h1>
+            {/* <p className="">{description}</p> */}
+            <SolarSystemBodyTable className="" headers={headers} rows={rows} />
+          </div>
+        )}
+        {solarSystemState === SolarSystemState.REJECTED && <p>ERROR: NOT LOADING</p>}
+      </section>
+    </Fragment>
   );
 };
